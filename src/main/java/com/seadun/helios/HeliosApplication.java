05e4de64d7d3d478916ca3b55588785815aa0e0f
sn_baby <sn_baby@qq.com>
2018-09-20 18:26:11 +0800

    新建

---
+++ src/main/java/com/seadun/helios/HeliosApplication.java
@@ -0,0 +1,12 @@
+package com.seadun.helios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliosApplication.class, args);
+	}
+}
