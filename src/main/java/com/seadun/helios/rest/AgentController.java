ce28d057a0087b54ae55c283cf59c4cc46555321
sn_baby <sn_baby@qq.com>
2018-09-21 12:33:55 +0800

    新增modbus模块

--- src/main/java/com/seadun/helios/rest/AgentController.java
+++ src/main/java/com/seadun/helios/rest/AgentController.java
@@ -2,29 +2,52 @@ package com.seadun.helios.rest;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.seadun.helios.entity.BaseUser;
-import com.seadun.helios.mapper.BaseUserMapper;
-
-import lombok.extern.slf4j.Slf4j;
+import com.serotonin.modbus4j.ModbusFactory;
+import com.serotonin.modbus4j.ModbusMaster;
+import com.serotonin.modbus4j.code.DataType;
+import com.serotonin.modbus4j.exception.ErrorResponseException;
+import com.serotonin.modbus4j.exception.ModbusInitException;
+import com.serotonin.modbus4j.exception.ModbusTransportException;
+import com.serotonin.modbus4j.ip.IpParameters;
+import com.serotonin.modbus4j.locator.BaseLocator;
 
 @Controller
 @RequestMapping("/api")
-@Slf4j
 public class AgentController {
-	
-	@Autowired
-	private BaseUserMapper baseUserMapper;
-	
+
 	@GetMapping(value = { "/test" })
 	@ResponseBody
-	public void clientData(HttpServletRequest req) {
-		BaseUser baseUser = baseUserMapper.selectByPrimaryKey("1");
-		log.debug(">>>>>测试数据,baseUserMapper:{}", baseUserMapper.selectByPrimaryKey("1"));
+	public void clientData(HttpServletRequest req)
+			throws ModbusTransportException, ErrorResponseException, ModbusInitException {
+		IpParameters params = new IpParameters();
+		params.setHost("192.168.2.249");
+		params.setPort(7011);
+		params.setEncapsulated(true);
+
+		ModbusMaster master = new ModbusFactory().createTcpMaster(params, true);// TCP
+		master.setTimeout(10000);
+		master.init();
+		BaseLocator<Number> loc = BaseLocator.holdingRegister(1, 0, DataType.FOUR_BYTE_BCD);
+		Number num = master.getValue(loc);
+
+	}
+	
+	public static void main(String[] args) throws ModbusInitException, ModbusTransportException, ErrorResponseException {
+		IpParameters params = new IpParameters();
+		params.setHost("192.168.2.249");
+		params.setPort(7011);
+		params.setEncapsulated(true);
+
+		ModbusMaster master = new ModbusFactory().createTcpMaster(params, true);// TCP
+		master.setTimeout(10000);
+		master.init();
+		BaseLocator<Number> loc = BaseLocator.holdingRegister(1, 0, DataType.FOUR_BYTE_BCD);
+		Number num = master.getValue(loc);
+		System.out.println(num);
 	}
 }
