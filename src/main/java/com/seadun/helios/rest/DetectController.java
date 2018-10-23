72894ab3b080f58d5d285e4ad67bb39a224ec88f
sn_baby <sn_baby@qq.com>
2018-10-23 17:10:59 +0800

    修改
    修改

--- src/main/java/com/seadun/helios/rest/DetectController.java
+++ src/main/java/com/seadun/helios/rest/DetectController.java
@@ -43,34 +43,49 @@ public class DetectController {
 				detectService.page(pageNum, pageSize, code, name, ip));
 		return new ResponseEntity<>(responseResult, HttpStatus.OK);
 	}
-	
+
 	@PostMapping(value = { "/add" })
 	@ResponseBody
-	public ResponseEntity<ResponseSuccessResult> add(HttpServletRequest request,@RequestBody JSONObject jsb) {
+	public ResponseEntity<ResponseSuccessResult> add(HttpServletRequest request, @RequestBody JSONObject jsb) {
 		log.debug("jsb:{}", jsb);
 		String name = jsb.getString("name");
 		String code = jsb.getString("code");
 		String ip = jsb.getString("ip");
 
-		detectService.addDetect(name, code, ip,request.getSession().getAttribute("userId").toString());
-		
-		logService.addLog(request, "添加侦测器："+jsb.toJSONString());
+		detectService.addDetect(name, code, ip, request.getSession().getAttribute("userId").toString());
+
+		logService.addLog(request, "添加侦测器：" + jsb.toJSONString());
 		ResponseSuccessResult responseResult = new ResponseSuccessResult(HttpStatus.OK.value(), "success");
 		return new ResponseEntity<>(responseResult, HttpStatus.OK);
 	}
-	
+
+	@PostMapping(value = { "/edit" })
+	@ResponseBody
+	public ResponseEntity<ResponseSuccessResult> edit(HttpServletRequest request, @RequestBody JSONObject jsb) {
+		log.debug("jsb:{}", jsb);
+		String name = jsb.getString("name");
+		String id = jsb.getString("id");
+		String ip = jsb.getString("ip");
+
+		detectService.editDetect(name, id, ip, request.getSession().getAttribute("userId").toString());
+
+		logService.addLog(request, "修改加侦测器：" + jsb.toJSONString());
+		ResponseSuccessResult responseResult = new ResponseSuccessResult(HttpStatus.OK.value(), "success");
+		return new ResponseEntity<>(responseResult, HttpStatus.OK);
+	}
+
 	@DeleteMapping(value = { "/del" })
 	@ResponseBody
-	public ResponseEntity<ResponseSuccessResult> del(HttpServletRequest request,@RequestBody JSONObject jsb) {
+	public ResponseEntity<ResponseSuccessResult> del(HttpServletRequest request, @RequestBody JSONObject jsb) {
 		log.debug("jsb:{}", jsb);
 		String id = jsb.getString("id");
 
 		detectService.deleteDetect(id);
-		logService.addLog(request, "删除侦测器："+jsb.toJSONString());
+		logService.addLog(request, "删除侦测器：" + jsb.toJSONString());
 		ResponseSuccessResult responseResult = new ResponseSuccessResult(HttpStatus.OK.value(), "success");
 		return new ResponseEntity<>(responseResult, HttpStatus.OK);
 	}
-	
+
 	@GetMapping(value = { "/list" })
 	@ResponseBody
 	public ResponseEntity<ResponseSuccessResult> list() {
--- src/main/java/com/seadun/helios/service/DetectService.java
+++ src/main/java/com/seadun/helios/service/DetectService.java
@@ -30,16 +30,16 @@ public class DetectService {
 	private DetectPcRelationMapper detectPcRelationMapper;
 	@Autowired
 	private AlarmMapper alarmMapper;
-	
+
 	@Transactional
-	public PageInfo<Detect> page(int pageNum,int pageSize,String code,String name,String ip) {
+	public PageInfo<Detect> page(int pageNum, int pageSize, String code, String name, String ip) {
 		RowBounds rowBounds = new RowBounds(pageNum, pageSize);
-		
-		List<Detect> detectList = detectMapper.selectPage(rowBounds,code,name,ip);
+
+		List<Detect> detectList = detectMapper.selectPage(rowBounds, code, name, ip);
 		PageInfo<Detect> pageInfo = new PageInfo<Detect>(detectList);// 封装分页信息，便于前端展示
 		return pageInfo;
 	}
-	
+
 	public void addDetect(String name, String code, String ip, String crtUser) {
 		Detect oldDetect = detectMapper.selectByCode(code);
 		if (oldDetect != null) {
@@ -47,16 +47,16 @@ public class DetectService {
 					HeliosExceptionConstants.DETECT_EXIST_EXCEPTION_MESSAGE,
 					HeliosExceptionConstants.DETECT_EXIST_EXCEPTION_HTTP_STATUS);
 		}
-		
+
 		Detect oldIpDetect = detectMapper.selectByIp(ip);
 		if (oldIpDetect != null) {
 			throw new HeliosException(HeliosExceptionConstants.IP_EXIST_EXCEPTION_CODE,
 					HeliosExceptionConstants.IP_EXIST_EXCEPTION_MESSAGE,
 					HeliosExceptionConstants.IP_EXIST_EXCEPTION_HTTP_STATUS);
 		}
-		
+
 		String detectId = UUID.randomUUID().toString();
-		
+
 		Detect detect = new Detect();
 		detect.setCode(code);
 		detect.setCrtTime(new Date());
@@ -65,37 +65,46 @@ public class DetectService {
 		detect.setIp(ip);
 		detect.setName(name);
 		detect.setStatus("0");
-		
+
 		detectMapper.insertSelective(detect);
-		
+
 		for (short i = 1; i <= 16; i++) {
 			DetectPort detectPort = new DetectPort();
-			detectPort.setCode(code+"_U"+i);
+			detectPort.setCode(code + "_U" + i);
 			detectPort.setCrtTime(new Date());
 			detectPort.setCrtUser(crtUser);
 			detectPort.setDetectId(detectId);
 			detectPort.setId(UUID.randomUUID().toString());
-			detectPort.setName("U"+i);
-			detectPort.setPort((short) (HeliosConstants.BASE_PORT+i));
+			detectPort.setName("U" + i);
+			detectPort.setPort((short) (HeliosConstants.BASE_PORT + i));
 			detectPort.setStatus("-1");
-			
+
 			detectPortMapper.insertSelective(detectPort);
 		}
-		
+
 	}
-	
+
+	public void editDetect(String name, String id, String ip, String crtUser) {
+		Detect detect = detectMapper.selectByPrimaryKey(id);
+		detect.setName(name);
+		detect.setIp(ip);
+		detect.setUptTime(new Date());
+		detect.setUptUser(crtUser);
+		detectMapper.updateByPrimaryKey(detect);
+	}
+
 	@Transactional
 	public void deleteDetect(String id) {
 		detectMapper.deleteByPrimaryKey(id);
-		
+
 		List<DetectPort> detectPortList = detectPortMapper.list(id);
-		detectPortList.forEach(detectPort->{
+		detectPortList.forEach(detectPort -> {
 			detectPcRelationMapper.deleteByPortId(detectPort.getId());
 			alarmMapper.deleteByPortId(detectPort.getId());
 			detectPortMapper.deleteByPrimaryKey(detectPort.getId());
 		});
 	}
-	
+
 	@Transactional
 	public List<Detect> list() {
 		return detectMapper.list();
